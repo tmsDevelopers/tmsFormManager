fa90d6297f1155c7381d8440bc328d866682046d
Антон Андреевич Морозов <chipset@rambler.ru>
2011-05-03 00:36:52 +0400

    Сделал html вывод text поля

--- libs/tmsBaseField.php
+++ libs/tmsBaseField.php
@@ -13,38 +13,84 @@ abstract  class BaseField{
     protected $TYPE = '';
     protected $VALUE = '';
 
-    abstract public function LoadConfig(array $config=array());
 
+    abstract public function LoadConfig(array $config=array());
+    abstract public function getHTML() ; // get html code of
+    abstract public function setValue(); // Метод задаёт значение параметра value элемента
+
+    /**
+     * метод возвращает строку с параметрами наиболее общими для элементов (name, id, class...)
+     * @return string
+     */
+    public function getBaseHTMLparametrs()
+    {
+        $result = '';
+        if($this->CLASS!='') $result .= ' class="'.$this->CLASS.'" ';
+
+        if($this->ID!='') $result .= ' id="'.$this->ID.'" ';
+
+        if($this->NAME!='')$result .= ' name="'.$this->NAME.'" ';
+        else $result.= ' name="'.$this->ID.'" ';
+
+        return $result;
+    }
+
+
+    /**
+     * Метод возвращает значение параметра class
+     * @return string or false
+     */
     public function getClass()
     {
         if($this->CLASS=='')return false;
         else return $this->CLASS;
     }
 
+    /**
+     * Метод возвращает значение параметра id элемента
+     * @return string or false
+     */
     public function getId()
     {
         if($this->ID=='')return false;
         else return $this->ID;
     }
 
+    /**
+     * Метод возвращает значение параметра name  элемента
+     * @return string or false
+     */
     public function getName()
     {
         if($this->NAME=='')return false;
         else return $this->NAME;
     }
 
+    /**
+     * метод возвращает тип элемента
+     * @return string or false
+     */
     public function getType()
     {
         if($this->TYPE=='')return false;
         else return $this->TYPE;
     }
 
+    /**
+     * метод возвращает значение элемента в установленном для него формате
+     * @return mixed
+     */
     public function getValue()
     {
         return $this->VALUE;
     }
 
-    public function setClass(string $class=NULL)
+    /**
+     * Метод задаёт значение параметра class
+     * @param string $class
+     * @return boolean
+     */
+    public function setClass($class=NULL)
     {
         if($class == NULL)return false;
         $class = trim($class);
@@ -57,7 +103,12 @@ abstract  class BaseField{
         return true;
     }
 
-   public function setId(string $id=NULL)
+   /**
+    * Метод задаёт значение id элемента (+ если на задан параметр name, то name=id)
+    * @param string $id
+    * @return boolean
+    */
+   public function setId( $id=NULL)
     {
        if($id == NULL)return false;
         $id = trim($id);
@@ -73,7 +124,12 @@ abstract  class BaseField{
         return true;
     }
 
-    public function setName(string $Name=NULL)
+    /**
+     * Метод устанавливает значение параметра name
+     * @param string $Name
+     * @return boolean
+     */
+    public function setName( $Name=NULL)
     {
         if($name == NULL)return false;;
         $name = trim($name);
@@ -100,11 +156,5 @@ abstract  class BaseField{
     }
     */
     
-    public function setValue(string $value= NULL)
-    {
-        $this->VALUE = $value;
-        return true;
-    }
-
 }
 ?>
--- libs/tmsForm.php
+++ libs/tmsForm.php
@@ -12,17 +12,66 @@ namespace tmsFormManager;
 class Form {
 
     protected  $CONFIG = array() ;  // array of configuration parametrs
+    protected $FIELDS = array() ; // array of fieldobjects
 
+
+    /**
+     * метод устанавливает конфигурационные параметры
+     * @param array $config
+     * @return boolean
+     */
     public function setConfig($config = array())
     {
         if(!is_array($config))return false;
 
         $this->CONFIG = $config;
+
         return true;
     }
 
+    /**
+     * Метод создаёт форму из полей на основании переданной конфигурации
+     */
     public function buildForm()
     {
+        $n = count($this->CONFIG['fields']);
+        if($n>0)
+        {
+            foreach( $this->CONFIG['fields'] as $field_config)
+            {
+                if($field_config['type'] != '')
+                {
+                    $field_class = $field_config['type'];
+                    $field_class = ucfirst(strtolower($field_class)).'Field';
+
+                    $field_class = '\\tmsFormManager\\'.$field_class;
+                    if(\class_exists($field_class))
+                    {
+                        $field = new $field_class() ;
+                        
+                        $field->LoadConfig($field_config);
+                         
+                        $this->FIELDS[$field->getId()] = $field;
+                    }
+
+                }
+            }
+        }
+        
+    }
+
+    /**
+     * Метод возвращает html код для поля по его id
+     * @param string $id
+     * @return string
+     */
+    public function getHTMLfield($id=NULL)
+    {
+        if($id==NULL)return false;
+        if(!\key_exists($id, $this->FIELDS))return false;
+
+        return $this->FIELDS[$id]->getHTML();
+
         
     }
 
